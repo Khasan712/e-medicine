dea4d0852d8d91718925d558fa95a10135887ad8
h.kamalov <k.kamalov@pegb.tech>
2025-02-08 17:12:34 +0500

    first comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py runserver 0.0.0.0:9092
-#gunicorn config.wsgi:application --bind 0.0.0.0:9092 --workers 2 --timeout 120
+#python manage.py runserver 0.0.0.0:9092
+gunicorn config.wsgi:application --bind 0.0.0.0:9092 --workers 2 --timeout 120
 
